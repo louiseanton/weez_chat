f4cef36d31f9fe3984085122b648e42f7bea455b
unknown <t06la11@.uoa.abdn.ac.uk>
2012-05-09 12:35:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeezChat::Application.config.secret_token = 'f826e715ecb353f1cb76aec869de274edf8c496142947ab3d9887a761548560169c48d6dc956ea4a17ccba5edbc386579a470eaef6843fd0d82d5e88bd9cc994'

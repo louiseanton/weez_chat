52774087ddd1bc0ca08dbb9e9b75509bd94350c4
unknown <t06la11@.uoa.abdn.ac.uk>
2012-05-09 16:18:20 +0100

    User model with passwords and user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 WeezChat::Application.routes.draw do
+  resources :users
+
+  match '/signup',  :to => 'users#new'
+
   get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  root :to => "pages#home"
 
 
   # The priority is based upon order of creation:
@@ -53,7 +59,7 @@ WeezChat::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   
 
   # See how all your routes lay out with "rake routes"
 

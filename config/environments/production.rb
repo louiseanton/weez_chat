b3573d89de6c68e5b97c6f3afe26d5c1e97519c9
unknown <t06la11@.uoa.abdn.ac.uk>
2012-05-10 13:11:38 +0100

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,11 @@ WeezChat::Application.configure do
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
+    # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
+
+  
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
--- db/migrate/20120509140850_add_password_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddPasswordToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :encypted_password, :string
-  end
-
-  def self.down
-    remove_column :users, :encypted_password
-  end
-end
